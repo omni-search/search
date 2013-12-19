9fdc904b9114381bc1e8c8a55a2fd3eeece2a516
RobertTheKing <robertkng104@gmail.com>
2013-12-19 21:56:04 +0000

    Fixes #4 .Empty search bar causes warning and refuses to search

--- app.rb
+++ app.rb
@@ -11,10 +11,12 @@ class App
 		searchterm = request.params['q']
 		#return request.params.inspect
 		string = '<html>' + CommonHTML.getHead(cards = true) # get head, including cards elements
-		string += '<body>' + CommonHTML.getHeader(searchPreload = searchterm) # get the header bar, and add the preloaded search value
-		if !searchterm
-			string += 'You gave no searchterm'
+		
+		if searchterm == ""
+			string += '<script> alert("You didnt search for anything")</script>'
+			string += '<script> window.location.href = "/home";</script>'
 		else 
+		  string += '<body>' + CommonHTML.getHeader(searchPreload = searchterm) # get the header bar, and add the preloaded search value
 			cards = Cards.getCards searchterm # get all cards
 			cards.each do |card|
 				string += card
